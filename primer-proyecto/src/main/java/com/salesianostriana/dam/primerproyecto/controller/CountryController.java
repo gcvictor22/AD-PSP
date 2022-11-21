4024285e2b3606013dcf734ca983901a483ebee9
gcvictor22 <victorgonzaleztr22@triana.salesianos.edu>
2022-11-21 11:47:34 +0100

    Get Post Put Delete

--- primer-proyecto/src/main/java/com/salesianostriana/dam/primerproyecto/controller/CountryController.java
+++ primer-proyecto/src/main/java/com/salesianostriana/dam/primerproyecto/controller/CountryController.java
@@ -3,12 +3,12 @@ package com.salesianostriana.dam.primerproyecto.controller;
 import com.salesianostriana.dam.primerproyecto.model.Country;
 import com.salesianostriana.dam.primerproyecto.repository.CountryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +18,7 @@ public class CountryController {
 
     @GetMapping("/country")
     //public List<Country> findAll(){
-    public ResponseEntity<List<Country>> findAll(){
+    public ResponseEntity<List<Country>> findAll() {
         //return repo.findAll();
         return ResponseEntity.ok(repo.findAll());
     }
@@ -26,8 +26,41 @@ public class CountryController {
     @GetMapping("/country/{code}")
     public ResponseEntity<Country> findByCode(
             @PathVariable String code
-    ){
+    ) {
         return ResponseEntity.of(repo.findFirstByCode(code));
+    }
+
+    @PostMapping("/country")
+    public ResponseEntity<Country> newCountry(@RequestBody Country country) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(repo.save(country));
+    }
+
+    @PutMapping("/country/{id}")
+    public ResponseEntity<Country> updateCountry(@RequestBody Country country, @PathVariable Long id) {
+
+        return ResponseEntity.of(
+                repo.findById(id)
+                        .map(old -> {
+                            old.setCapital(country.getCapital());
+                            old.setCode(country.getCode());
+                            old.setName(country.getName());
+                            old.setCurrency(country.getCurrency());
+                            old.setPopulation(country.getPopulation());
+                            return Optional.of(repo.save(old));
+                        })
+                        .orElse(Optional.empty())
+                //.orElseThrow(() -> new CountryNotFoundException(...)); Codigo correcto
+        );
+
+    }
+
+    @DeleteMapping("/country/{id}")
+    public ResponseEntity<Country> deleteCountry(@PathVariable Long id){
+        if(repo.existsById(id))
+            repo.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
