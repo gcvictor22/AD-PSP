d3a26c241cd49fc956a4c9365e46a252fddc110f
gcvictor22 <victorgonzaleztr22@gmail.com>
2023-01-23 22:29:21 +0100

    SearchCriteria Pageable

---
+++ ejercicio-rest-query-master/src/main/java/com/salesianostriana/dam/restquery/dto/product/ProductDto.java
@@ -0,0 +1,30 @@
+package com.salesianostriana.dam.restquery.dto.product;
+
+import com.salesianostriana.dam.restquery.Product;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductDto {
+
+    private String name;
+    private LocalDate releaseDate;
+    private Double price;
+    private boolean available;
+
+    public static ProductDto of (Product p){
+        return ProductDto.builder()
+                .name(p.getProductName())
+                .releaseDate(p.getReleaseDate())
+                .price(p.getPrice())
+                .available(p.isAvailable())
+                .build();
+
+    }
+
+}
